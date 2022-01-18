d3831e28ac54c115046aa6ac16ba0da87c3acc13
Vinnie Marone <vinnie.marone35@gmail.com>
2022-01-18 03:05:11 -0500

    Bootstrap initial project

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+})
+export default class Home extends Vue {}
+</script>
